3cba299a87663d3c09c657695e0cfa402171e85b
gefgu <gefgu@hotmail.com>
2022-06-04 19:21:39 -0300

    Add general layout in  react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import "./styles/app.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <section>
+        <h2>About Me</h2>
+      </section>
+      <section>
+        <h2>My work</h2>
+      </section>
+      <section>
+        <h2>Contact me</h2>
+      </section>
+    </div>
+  );
 }
 
 export default App;
