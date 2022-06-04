36d431f54c624ecfaa225ef5c599937817cc5202
gefgu <gefgu@hotmail.com>
2022-06-04 19:33:33 -0300

    Add project card html

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,15 @@
+const ProjectCard = () => {
+  return (
+    <article className="project-card">
+      <img src="https://picsum.photos/200/300" alt="placeholder" />
+      <section className="project-description">
+        <h4>Project Name</h4>
+        <p>
+          Short descrpition of the project. Just a couple sentences will do.
+        </p>
+      </section>
+    </article>
+  );
+};
+
+export default ProjectCard;
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,9 +1,17 @@
+import ProjectCard from "./ProjectCard";
+
 const WorkSection = () => {
   return (
     <section className="work">
       <h2>My work</h2>
       <section className="projects">
         <h3 className="hidden">Projects</h3>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </section>
     </section>
   );
