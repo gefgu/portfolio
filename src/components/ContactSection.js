b680c254c11e1ad8224ec9a59e8f34071e485e0e
gefgu <gefgu@hotmail.com>
2022-06-04 19:56:28 -0300

    Add contact info html layout

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,7 +1,18 @@
 const ContactSection = () => {
   return (
-    <section>
+    <section className="contact">
       <h2>Contact me</h2>
+      <p>
+        Please get in touch if you think our work could be mutually beneficial!
+      </p>
+      <address>
+        1234 Random Road <br /> Random Town, California 12345
+      </address>
+      <section className="contact-info">
+        <address>555-555-5555</address>
+        <address>asheleywilliams.is.not.real@gmail.com</address>
+      </section>
+      <img src="https://picsum.photos/800" alt="placeholder" />
     </section>
   );
 };
