374be8aeb9bb6f1f5038b0c1ffbff064075a951c
gefgu <gefgu@hotmail.com>
2022-06-04 20:52:16 -0300

    Add positioning for contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,18 +1,25 @@
+import "../styles/contactSection.css";
+
 const ContactSection = () => {
   return (
     <section className="contact">
-      <h2>Contact me</h2>
-      <p>
-        Please get in touch if you think our work could be mutually beneficial!
-      </p>
-      <address>
-        1234 Random Road <br /> Random Town, California 12345
-      </address>
-      <section className="contact-info">
-        <address>555-555-5555</address>
-        <address>asheleywilliams.is.not.real@gmail.com</address>
+      <section>
+        <h2>Contact me</h2>
+        <p>
+          Please get in touch if you think our work could be mutually
+          beneficial!
+        </p>
+        <address>
+          1234 Random Road <br /> Random Town, California 12345
+        </address>
+        <section className="contact-info">
+          <address>555-555-5555</address>
+          <address>asheleywilliams.is.not.real@gmail.com</address>
+        </section>
       </section>
-      <img src="https://picsum.photos/800" alt="placeholder" />
+      <picture>
+        <img src="https://picsum.photos/600/400" alt="placeholder" />
+      </picture>
     </section>
   );
 };
