6cb27f8daaf3f57a89e5c149611c4e37a777e7e0
gefgu <gefgu@hotmail.com>
2022-06-04 19:49:41 -0300

    Refactor contact in separate component and comment work section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./styles/app.css";
 import WorkSection from "./components/WorkSection";
+import ContactSection from "./components/ContactSection";
 
 function App() {
   return (
     <div className="container">
       <section>
         <h2>About Me</h2>
-        <WorkSection />
-        <h2>Contact me</h2>
+        {/* <WorkSection /> */}
+        <ContactSection />
       </section>
     </div>
   );
---
+++ src/components/ContactSection.js
@@ -0,0 +1,9 @@
+const ContactSection = () => {
+  return (
+    <section>
+      <h2>Contact me</h2>
+    </section>
+  );
+};
+
+export default ContactSection;
