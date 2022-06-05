4fece82c0d0eb3ad81fbb3b2e5b71171c8454450
gefgu <gefgu@hotmail.com>
2022-06-05 09:43:30 -0300

    Add about section html

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,21 @@
 const AboutSection = () => {
   return (
-    <section>
-      <h2>About Me</h2>
-    </section>
+    <article className="about">
+      <div className="profile">
+        <img src="https://picsum.photos/400" alt="placeholder" />
+        <h1>Ashely Williams</h1>
+      </div>
+      <section className="description">
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et quam
+          sed ipsum hendrerit dapibus. Phasellus imperdiet feugiat quam sed
+          elementum. Aliquam erat volutpat. Suspendisse pulvinar, felis luctus
+          interdum vulputate, magna urna pulvinar erat, accumsan venenatis urna
+          nisl eget justo.{" "}
+        </p>
+      </section>
+    </article>
   );
 };
 
