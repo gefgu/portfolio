8a227d7a39ed6504a90006de763edc3ef70945da
gefgu <gefgu@hotmail.com>
2022-06-06 20:55:14 -0300

    Add icons in project description

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,16 @@ const ProjectCard = () => {
     <article className="project-card">
       <img src="https://picsum.photos/600" alt="placeholder" />
       <section className="project-description">
-        <h4>Project Name</h4>
+        <div className="project-heading">
+          <h4>Project Name</h4>
+          <div className="icons">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+              alt="github"
+            />
+            <img src={require("../icons/open-in-new.png")} alt="live demo" />
+          </div>
+        </div>
         <p>
           Short description of the project. Just a couple sentences will do.
         </p>
