1a1e65ad18100291c2e42bb2e986b3432a9f8300
gefgu <gefgu@hotmail.com>
2022-06-10 16:53:25 -0300

    Add contact info personal info

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -11,17 +11,35 @@ const ContactSection = () => {
           Please get in touch if you think our work could be mutually
           beneficial!
         </p>
-        <address>
-          1234 Random Road <br /> Random Town, California 12345
-        </address>
         <section className="contact-info">
           <address>
-            <BiPhone /> 555-555-5555
-          </address>
-          <address>
-            <RiMailLine /> asheleywilliams.is.not.real@gmail.com
+            <a href="mailto:gefgu12@gmail.com">
+              <RiMailLine /> gefgu12@gmail.com
+            </a>
           </address>
         </section>
+        <div className="icons">
+          <a href="https://github.com/gefgu">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+              alt="github"
+            />
+          </a>
+
+          <a href="https://www.linkedin.com/in/gustavobruno-developer/">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+              alt="linkedin"
+            />
+          </a>
+
+          <a href="https://twitter.com/codewithgustavo">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg"
+              alt="twitter"
+            />
+          </a>
+        </div>
       </section>
       <picture>
         <img src="https://picsum.photos/900/500" alt="placeholder" />
